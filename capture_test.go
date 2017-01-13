229953fef60df17e32d3c632e5185542bfd0e681
Scott Oberg <scott.oberg@sri.com>
2017-01-13 11:09:54 -0800

    issue fixes #1 Capture and CaptureWithCGo leak file descriptors on OSX

--- capture_test.go
+++ capture_test.go
@@ -1,13 +1,29 @@
 package osutil
 
 import (
+	"syscall"
 	"testing"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCapture(t *testing.T) {
-	testCapture(t)
+	var limit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit)
+	assert.Nil(t, err)
+	//fmt.Fprintf(os.Stderr, "%v file descriptors out of a maximum of %v available\n", limit.Cur, limit.Max) 
+	// use limit.Cur or something like 1024
+	for i :=0; i <= int(limit.Cur); i++ {
+		testCapture(t)
+	}
 }
 
 func TestCaptureWithCGo(t *testing.T) {
-	testCaptureWithCGo(t)
+	var limit syscall.Rlimit
+	err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit)
+	assert.Nil(t, err)
+	//fmt.Fprintf(os.Stderr, "%v file descriptors out of a maximum of %v available\n", limit.Cur, limit.Max) 
+	// use limit.Cur or something like 512
+	for i:=0; i <= int(limit.Cur); i++ {
+		testCaptureWithCGo(t)
+	}
 }
