a2f4baaa2ac2ca2b69bc843cac52f0219b6441ec
Scott Oberg <scott.oberg@sri.com>
2017-01-13 11:09:54 -0800

    Capture and CaptureWithCGo leak file descriptors

--- capture_test.go
+++ capture_test.go
@@ -1,13 +1,28 @@
 package osutil
 
 import (
+	"syscall"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCapture(t *testing.T) {
-	testCapture(t)
+	var limit syscall.Rlimit
+	assert.NoError(t, syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit))
+
+	// Use at least one more file descriptor than our current limit, so we make sure that there are no file descriptor leaks.
+	for i := 0; i <= int(limit.Cur); i++ {
+		testCapture(t)
+	}
 }
 
 func TestCaptureWithCGo(t *testing.T) {
-	testCaptureWithCGo(t)
+	var limit syscall.Rlimit
+	assert.NoError(t, syscall.Getrlimit(syscall.RLIMIT_NOFILE, &limit))
+
+	// Use at least one more file descriptor than our current limit, so we make sure that there are no file descriptor leaks.
+	for i := 0; i <= int(limit.Cur); i++ {
+		testCaptureWithCGo(t)
+	}
 }
