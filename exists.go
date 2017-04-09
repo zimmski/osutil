25b263b13a6719665ea3fd6869475e958af75275
Markus Zimmermann <markus.zimmermann@nethead.at>
2017-04-09 17:15:33 +0200

    Stat and FileExists

--- exists.go
+++ exists.go
@@ -5,16 +5,18 @@ import (
 	"os"
 )
 
-// Exist errors
 var (
+	// ErrNotADirectory the given directory does not exist
 	ErrNotADirectory = errors.New("not a directory")
+	// ErrNotAFile the given file does not exist
+	ErrNotAFile = errors.New("not a file")
 )
 
-// DirExists check if a directory exists
-func DirExists(d string) error {
-	f, err := os.Open(d)
+// Stat retuns a FileInfo structure describing the given file
+func Stat(filepath string) (os.FileInfo, error) {
+	f, err := os.Open(filepath)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer func() {
 		if err := f.Close(); err != nil {
@@ -24,11 +26,35 @@ func DirExists(d string) error {
 
 	fi, err := f.Stat()
 	if err != nil {
+		return nil, err
+	}
+
+	return fi, nil
+}
+
+// DirExists checks if a directory exists
+func DirExists(path string) error {
+	fi, err := Stat(path)
+	if err != nil {
 		return err
 	}
 
 	if !fi.Mode().IsDir() {
 		return ErrNotADirectory
+	}
+
+	return nil
+}
+
+// FileExists checks if a file exists
+func FileExists(filepath string) error {
+	fi, err := Stat(filepath)
+	if err != nil {
+		return err
+	}
+
+	if fi.Mode().IsDir() {
+		return ErrNotAFile
 	}
 
 	return nil
