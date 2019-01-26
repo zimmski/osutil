5d4802ee8ea5bbb61a86e963d14c853a0b516876
Markus Zimmermann <markus.zimmermann@nethead.at>
2019-01-26 20:02:11 +0100

    More fine-grained locking so we can capture in a capture

--- capture.go
+++ capture.go
@@ -14,16 +14,17 @@ import (
 	"syscall"
 )
 
-var lockStdFileDescriptorsSwapping sync.Mutex // FIXME our solution is not concurrent-safe. Find a better solution because this might be a bottleneck in the future.
+var lockStdFileDescriptorsSwapping sync.Mutex
 
 // Capture captures stderr and stdout of a given function call.
 func Capture(call func()) (output []byte, err error) {
-	lockStdFileDescriptorsSwapping.Lock()
-	defer lockStdFileDescriptorsSwapping.Unlock()
-
 	originalStdErr, originalStdOut := os.Stderr, os.Stdout
 	defer func() {
+		lockStdFileDescriptorsSwapping.Lock()
+
 		os.Stderr, os.Stdout = originalStdErr, originalStdOut
+
+		lockStdFileDescriptorsSwapping.Unlock()
 	}()
 
 	r, w, err := os.Pipe()
@@ -37,7 +38,11 @@ func Capture(call func()) (output []byte, err error) {
 		}
 	}()
 
+	lockStdFileDescriptorsSwapping.Lock()
+
 	os.Stderr, os.Stdout = w, w
+
+	lockStdFileDescriptorsSwapping.Unlock()
 
 	out := make(chan []byte)
 	go func() {
@@ -64,31 +69,48 @@ func Capture(call func()) (output []byte, err error) {
 // CaptureWithCGo captures stderr and stdout as well as stderr and stdout of C of a given function call.
 func CaptureWithCGo(call func()) (output []byte, err error) {
 	lockStdFileDescriptorsSwapping.Lock()
-	defer lockStdFileDescriptorsSwapping.Unlock()
 
 	originalStdout, e := syscall.Dup(syscall.Stdout)
 	if e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
 
 	originalStderr, e := syscall.Dup(syscall.Stderr)
 	if e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
 
+	lockStdFileDescriptorsSwapping.Unlock()
+
 	defer func() {
+		lockStdFileDescriptorsSwapping.Lock()
+
 		if e := syscall.Dup2(originalStdout, syscall.Stdout); e != nil {
+			lockStdFileDescriptorsSwapping.Unlock()
+
 			err = e
 		}
 		if e := syscall.Close(originalStdout); e != nil {
+			lockStdFileDescriptorsSwapping.Unlock()
+
 			err = e
 		}
 		if e := syscall.Dup2(originalStderr, syscall.Stderr); e != nil {
+			lockStdFileDescriptorsSwapping.Unlock()
+
 			err = e
 		}
 		if e := syscall.Close(originalStderr); e != nil {
+			lockStdFileDescriptorsSwapping.Unlock()
+
 			err = e
 		}
+
+		lockStdFileDescriptorsSwapping.Unlock()
 	}()
 
 	r, w, err := os.Pipe()
@@ -102,12 +124,20 @@ func CaptureWithCGo(call func()) (output []byte, err error) {
 		}
 	}()
 
+	lockStdFileDescriptorsSwapping.Lock()
+
 	if e := syscall.Dup2(int(w.Fd()), syscall.Stdout); e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
 	if e := syscall.Dup2(int(w.Fd()), syscall.Stderr); e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
+
+	lockStdFileDescriptorsSwapping.Unlock()
 
 	out := make(chan []byte)
 	go func() {
@@ -123,18 +153,28 @@ func CaptureWithCGo(call func()) (output []byte, err error) {
 
 	call()
 
+	lockStdFileDescriptorsSwapping.Lock()
+
 	C.fflush(C.stdout)
 
 	err = w.Close()
 	if err != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, err
 	}
 	if e := syscall.Close(syscall.Stdout); e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
 	if e := syscall.Close(syscall.Stderr); e != nil {
+		lockStdFileDescriptorsSwapping.Unlock()
+
 		return nil, e
 	}
+
+	lockStdFileDescriptorsSwapping.Unlock()
 
 	return <-out, err
 }
